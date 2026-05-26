42d35f59968c5fdda87d63d6367b79818fa54b46
jeevasanjaysv <jeevasanjay.s2024ece@sece.ac.in>
2026-05-26 15:55:15 +0530

    CRUD operation created in coursecountroller

--- src/main/java/com/eduhub/eduhub_backed/controler/CourseController.java
+++ src/main/java/com/eduhub/eduhub_backed/controler/CourseController.java
@@ -1,30 +1,83 @@
 package com.eduhub.eduhub_backed.controler;
 
-import com.eduhub.eduhub_backed.components.CourseService;
+import com.eduhub.eduhub_backed.components.Course;
 import com.eduhub.eduhub_backed.components.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class CourseController {
 
-    @Autowired
-    private CourseService service;
+    List<Course> courseList=new ArrayList<>();
 
-    @Autowired
-    private ProductService pro;
+    public  CourseController(){
+        courseList.add(new Course(1,"java",4));
+        courseList.add(new Course(2,"python",4));
+        courseList.add(new Course(3,"cpp",4));
+        courseList.add(new Course(4,"adc",4));
+        courseList.add(new Course(5,"mpes",4));
 
-    @GetMapping("/cours")
-    public String getcourse(){
-        String c=service.getCourse();
-        return  c;
     }
 
-    @GetMapping("/pro")
-    public  String products(){
-        return pro.getProduct();
+    @GetMapping("/courses")
+    public ResponseEntity<List<Course>> getCourses(){
+        return new ResponseEntity<>(courseList, HttpStatus.OK);
     }
 
+    @GetMapping("/courses/{id}")
+    public  ResponseEntity<Course> getCourseByCode(@PathVariable("id") int id){
+        for (int i = 0; i < courseList.size(); i++) {
+            if(courseList.get(i).getCourseCode()==id){
+                return new ResponseEntity<>(courseList.get(i),HttpStatus.OK);
+            }
+        }
+        return new ResponseEntity<>(courseList.get(0),HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping("/course/{id}/query")
+    public  ResponseEntity<Course> getCourseByPathAndQuery(@PathVariable("id") int id, @RequestParam String name){
+        for (int i = 0; i < courseList.size(); i++) {
+            if(courseList.get(i).getCourseCode()==id && courseList.get(i).getCourseName().equals(name)){
+                return new ResponseEntity<>(courseList.get(i),HttpStatus.OK);
+            }
+        }
+        return new ResponseEntity<>(courseList.get(0),HttpStatus.BAD_REQUEST);
+    }
+
+    @PostMapping("/addcourse")
+    public ResponseEntity addCourse(@RequestBody
+                                        Course course){
+
+        courseList.add(course);
+        return ResponseEntity.accepted().body("Added");
+    }
+
+
+    @PutMapping("{id}/{updatedName}")
+    public ResponseEntity updateCourse(@PathVariable("id") int id,
+                                       @PathVariable("updatedName") String name ){
+
+        for (int i = 0; i < courseList.size(); i++) {
+            if(courseList.get(i).getCourseCode()==id){
+                courseList.get(i).setCourseName(name);
+            }
+        }
+        return ResponseEntity.accepted().body("updated");
+    }
+
+    @DeleteMapping("{id}")
+    public  ResponseEntity deleteCourse(@PathVariable("id") int id){
+        for (int i = 0; i < courseList.size(); i++) {
+            if(courseList.get(i).getCourseCode()==id){
+               courseList.remove(i);
+            }
+        }
+        return ResponseEntity.accepted().body("deleted");
+    }
 
 }
